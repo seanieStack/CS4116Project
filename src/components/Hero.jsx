2a174d61b253626e5ebe2f90c25f9bea39c6acfb
seanieStack <22374302@studentmail.ul.ie>
2025-02-27 17:34:23 +0000

    updated to use semi-real stats, and import icons instead of using svgs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,7 @@
+"use client"
+
+import {ArrowRightIcon} from "@heroicons/react/24/outline";
+
 export default function Hero(){
     return (
         <section
@@ -14,12 +18,7 @@ export default function Hero(){
                     <div className="mt-10 flex items-center justify-center gap-x-6">
                         <a className="isomorphic-link isomorphic-link--internal inline-flex items-center justify-center gap-2 rounded-xl bg-blue-600 px-4 py-3 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600" href="/services">
                             Shop Now
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20"
-                                 fill="currentColor">
-                                <path fillRule="evenodd"
-                                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                      clipRule="evenodd"></path>
-                            </svg>
+                            <ArrowRightIcon className="h-6 w-6 text-white" />
                         </a>
                     </div>
                 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
+"use client"
 import ColorModeToggle from "@/components/ColorModeToggle";
+import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/outline";
 
 export default function Navbar() {
     return (
@@ -7,8 +9,8 @@ export default function Navbar() {
                 <a className="lg:order-1 flex-none text-xl font-semibold text-black dark:text-white focus:outline-none focus:opacity-80 px-4" href="#">Techs & The City</a>
                 <div className="lg:order-3 flex items-center gap-x-2">
                     <button type="button" className="lg:hidden hs-collapse-toggle relative size-7 flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" id="hs-navbar-alignment-collapse" aria-expanded="false" aria-controls="hs-navbar-alignment" aria-label="Toggle navigation" data-hs-collapse="#hs-navbar-alignment">
-                        <svg className="hs-collapse-open:hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
-                        <svg className="hs-collapse-open:block hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+                        <Bars3Icon className="hs-collapse-open:hidden shrink-0 "/>
+                        <XMarkIcon className="hs-collapse-open:block hidden shrink-0 "/>
                         <span className="sr-only">Toggle</span>
                     </button>
                     <a href="/login" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
