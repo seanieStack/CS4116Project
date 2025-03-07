0305810d1ba7617750edf8ef6bd27362a92d110e
seanieStack <22374302@studentmail.ul.ie>
2025-03-07 15:42:36 +0000

    maybe this will fix it

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+        ENV_AWS_ACCESS_KEY_ID: process.env.ENV_AWS_ACCESS_KEY_ID,
+        ENV_AWS_SECRET_ACCESS_KEY: process.env.ENV_AWS_SECRET_ACCESS_KEY,
+        ENV_AWS_S3_BUCKET_NAME: process.env.ENV_AWS_S3_BUCKET_NAME,
+        ENV_AWS_REGION: process.env.ENV_AWS_REGION
+    }
+};
 
 export default nextConfig;
