48629269ec37313d8673da492f94c589397140cf
PaudieKelly <22342842@studentmail.ul.ie>
2025-03-24 19:32:49 +0000

    With mock data only

--- src/app/businesspanel/messages/page.jsx
+++ src/app/businesspanel/messages/page.jsx
@@ -1,8 +1,14 @@
 import Navbar from "@/components/Navbar";
 import BPNav from "@/components/BPNav";
 import BPMessages from "@/components/BPMessages";
+import {getCurrentUser} from "@/auth/nextjs/currentUser";
+import {redirect} from "next/navigation";
 
-export default function messages() {
+export default async function messages() {
+    const user = await getCurrentUser();
+    /*if (user === null || user.role !== "BUSINESS") {
+        redirect("/")
+    } */
     return (
         <>
             <Navbar/>
