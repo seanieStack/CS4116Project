641e07816178b2b28eb4fb2d536012b5d77777c6
Adam.Herron <118226971+AdamH03@users.noreply.github.com>
2025-04-27 19:52:48 +0100

    Updated files that should work in light/dark mode.

--- src/app/admin/users/page.jsx
+++ src/app/admin/users/page.jsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/Navbar";
 import AdminNav from "@/components/APNav";
 import UsersList from "@/components/UsersList";
-import {prisma} from "@/lib/prisma";
 import Footer from "@/components/Footer";
 
 export default async function UsersPage() {
@@ -14,7 +13,7 @@ export default async function UsersPage() {
             <AdminNav/>
             <div
                 className="mx-auto min-h-[calc(100vh-11em)] px-6 py-12 bg-gradient-to-b from-blue-200 dark:from-blue-950 text-white">
-                <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+                <div className="bg-white dark:bg-gray-800 text-black dark:text-white p-6 rounded-lg shadow-lg">
                     <h2 className="text-2xl font-semibold mb-4">Users</h2>
                     <UsersList buyers={users}/>
                 </div>
